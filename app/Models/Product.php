798d9d21e8377a1042937b220a12796e64a90b51
M Rizki Arya Nanda <rizkiarya0611@gmail.com>
2026-01-15 15:50:08 +0700

    creat all table

---
+++ app/Models/Product.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+    public function tenant(){
+        return $this->belongsTo(Tenant::class);
+    }
+}
